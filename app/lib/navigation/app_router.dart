96c5eb287822389cae3c63d12ae0f7e225e4380a
Mihir Rawal <mihir.rawal@neosoftmail.com>
2024-07-17 11:45:23 +0530

    added routes for payment related pages

--- app/lib/navigation/app_router.dart
+++ app/lib/navigation/app_router.dart
@@ -1,4 +1,10 @@
+import 'package:app/feature/payments/payments.dart';
+import 'package:app/feature/payments_page/payments_page.dart';
+import 'package:app/feature/payments_page/payments_view_model.dart';
 import 'package:app/feature/tabbar/tabbar_page.dart';
+import 'package:app/molecules/payments/payment_details.dart';
+import 'package:app/molecules/payments_page.dart/cheque_page.dart';
+import 'package:app/molecules/payments_page.dart/coupon_list.dart';
 import 'package:flutter/cupertino.dart';
 
 import '../feature/splash/splash_page.dart';
@@ -15,7 +21,28 @@ class AppRouter {
         return CupertinoPageRoute(
             builder: (context) => const TabbarPage(),
             settings: const RouteSettings(name: RoutePaths.tabbar));
-
+      case RoutePaths.payments:
+        return CupertinoPageRoute(
+            builder: (context) => const Payments(),
+            settings: const RouteSettings(name: RoutePaths.payments));
+      case RoutePaths.paymentDetails:
+        return CupertinoPageRoute(
+            builder: (context) => const PaymentDetailScreen(),
+            settings: const RouteSettings(name: RoutePaths.paymentDetails));
+      case RoutePaths.paymentsPage:
+        return CupertinoPageRoute(
+            builder: (context) => const PaymentsPage(),
+            settings: const RouteSettings(name: RoutePaths.paymentsPage));
+      case RoutePaths.couponList:
+        return CupertinoPageRoute(
+            builder: (context) => const CouponList(),
+            settings: const RouteSettings(name: RoutePaths.couponList));
+      case RoutePaths.chequePayment:
+        return CupertinoPageRoute(
+            builder: (context) => Chequepage(
+                  pageModel: settings.arguments as PaymentsPageModel,
+                ),
+            settings: const RouteSettings(name: RoutePaths.chequePayment));
       default:
         // Replace by Empty Page
         return CupertinoPageRoute(
--- app/lib/navigation/route_paths.dart
+++ app/lib/navigation/route_paths.dart
@@ -3,4 +3,9 @@ class RoutePaths {
   static const String splash = '/splash';
   static const String registration = '/registration';
   static const String tabbar = '/tabbar';
+  static const String payments = '/payments';
+  static const String paymentDetails = '/paymentDetails';
+  static const String paymentsPage = '/paymentsPage';
+  static const String couponList = '/couponList';
+  static const String chequePayment = '/chequePayment';
 }
