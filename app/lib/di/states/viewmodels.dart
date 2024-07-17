93138e2291daddda03aaac1198c11a10464b0c1e
Mihir Rawal <mihir.rawal@neosoftmail.com>
2024-07-17 11:49:27 +0530

    added 2 providers for payment related pages

--- app/lib/di/states/viewmodels.dart
+++ app/lib/di/states/viewmodels.dart
@@ -1,4 +1,6 @@
 import 'package:app/feature/dashboard/dashbaord_view_model.dart';
+import 'package:app/feature/payments/payments_model.dart';
+import 'package:app/feature/payments_page/payments_view_model.dart';
 import 'package:app/feature/splash/splash_page_model.dart';
 import 'package:app/feature/tabbar/tabbar_view_model.dart';
 import 'package:flutter_errors/flutter_errors.dart';
@@ -20,4 +22,13 @@ final tabbarViewModelProvider =
 final dashboardViewModelProvider =
     ChangeNotifierProvider.autoDispose<DashboardPageModel>(
   (ref) => DashboardPageModel(getIt.get<FlutterExceptionHandlerBinder>()),
+);
+
+final paymentsModelProvider = ChangeNotifierProvider.autoDispose<PaymentsModel>(
+  (ref) => PaymentsModel(getIt.get<FlutterExceptionHandlerBinder>()),
+);
+
+final paymentsPageModelProvider =
+    ChangeNotifierProvider.autoDispose<PaymentsPageModel>(
+  (ref) => PaymentsPageModel(getIt.get<FlutterExceptionHandlerBinder>()),
 );
