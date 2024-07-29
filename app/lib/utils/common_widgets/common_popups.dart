319a660a92b7fec5d5ec74b5c2a61844f6ba422e
Mihir Rawal <mihir.rawal@neosoftmail.com>
2024-07-29 11:22:09 +0530

    common widgets updated and some new added

--- app/lib/utils/common_widgets/common_popups.dart
+++ app/lib/utils/common_widgets/common_popups.dart
@@ -2,7 +2,9 @@ import 'package:app/utils/app_typography.dart';
 import 'package:app/utils/common_widgets/common_elevated_button.dart';
 import 'package:app/utils/common_widgets/common_text_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:io' show Platform, exit;
 
 class CommonPopups {
   // Private constructor
@@ -17,46 +19,51 @@ class CommonPopups {
   }
 
   // Method to show a success popup
-  void showSuccess(BuildContext context, String message) {
-    _showDialog(
-      context,
-      icon: Icons.check_circle,
-      iconColor: Colors.green,
-      message: message,
-      buttonText: 'OK',
-    );
+  showSuccess(BuildContext context, String message,
+      Function(bool shouldRoute) onChanged) {
+    _showDialog(context,
+        icon: Icons.check_circle,
+        iconColor: Colors.green,
+        message: message,
+        buttonText: 'OK',
+        onChanged: onChanged);
   }
 
   // Method to show an error popup
-  void showError(BuildContext context, String message) {
-    _showDialog(
-      context,
-      icon: Icons.error,
-      iconColor: Colors.red,
-      message: message,
-      buttonText: 'OK',
-    );
+  void showError(BuildContext context, String message,
+      Function(bool shouldRoute) onChanged) {
+    _showDialog(context,
+        icon: Icons.error,
+        iconColor: Colors.red,
+        message: message,
+        buttonText: 'OK',
+        onChanged: onChanged);
   }
 
   // Method to show a warning popup
-  void showWarning(BuildContext context, String message) {
+  void showWarning(
+    BuildContext context,
+    String message,
+  ) {
     _showDialog(
       context,
       icon: Icons.warning,
       iconColor: Colors.orange,
       message: message,
       buttonText: 'OK',
+      onChanged: (shouldRoute) {},
     );
   }
 
+  void showAppClose(BuildContext context, String message) {}
+
   // Private method to show a dialog
-  void _showDialog(
-    BuildContext context, {
-    required IconData icon,
-    required Color iconColor,
-    required String message,
-    required String buttonText,
-  }) {
+  void _showDialog(BuildContext context,
+      {required IconData icon,
+      required Color iconColor,
+      required String message,
+      required String buttonText,
+      required Function(bool shouldRoute) onChanged}) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -86,12 +93,84 @@ class CommonPopups {
                   child: CommonElevatedButton(
                     onPressed: () {
                       Navigator.pop(context);
+                      onChanged(true);
                     },
                     text: 'Ok',
                     backgroundColor: Theme.of(context).colorScheme.primary,
                     textStyle: AppTypography.subtitle2.copyWith(
                         color: Theme.of(context).colorScheme.onPrimary),
                   ))
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Private method to show a dialog
+  void _showAppClosingAlert(
+    BuildContext context, {
+    required IconData icon,
+    required Color iconColor,
+    required String message,
+    required String buttonText,
+  }) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(
+                icon,
+                color: Theme.of(context).colorScheme.primary,
+                size: 60,
+              ),
+              const SizedBox(height: 15),
+              CommonText(
+                text: message,
+                textAlign: TextAlign.center,
+                style: AppTypography.subtitle1
+                    .copyWith(color: Theme.of(context).colorScheme.primary),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                      height: 40.h,
+                      width: 67.w,
+                      child: CommonElevatedButton(
+                        onPressed: () {
+                          if (Platform.isAndroid) {
+                            SystemNavigator.pop();
+                          } else if (Platform.isIOS) {
+                            exit(0);
+                          }
+                        },
+                        text: 'Yes',
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        textStyle: AppTypography.subtitle2.copyWith(
+                            color: Theme.of(context).colorScheme.onPrimary),
+                      )),
+                  SizedBox(
+                      height: 40.h,
+                      width: 67.w,
+                      child: CommonElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        text: '',
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        textStyle: AppTypography.subtitle2.copyWith(
+                            color: Theme.of(context).colorScheme.onPrimary),
+                      )),
+                ],
+              )
             ],
           ),
         );
