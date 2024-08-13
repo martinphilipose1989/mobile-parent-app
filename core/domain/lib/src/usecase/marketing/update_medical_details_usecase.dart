9337a195aaa7be86d7856f9f6a5089301f83da7b
KushDev28 <kushal.chaturvedi@neosoftmail.com>
2024-08-13 19:15:02 +0530

    registration api done

---
+++ core/domain/lib/src/usecase/marketing/update_medical_details_usecase.dart
@@ -0,0 +1,35 @@
+import 'package:domain/domain.dart';
+import 'package:domain/src/usecase/base/base_usecase.dart';
+import 'package:network_retrofit/network_retrofit.dart';
+
+class UpdateMedicalDetailsUsecase<T> extends BaseUseCase<BaseError,
+    UpdateMedicalDetailsUsecaseParams, BaseInfo<T>> {
+  final RegistrationRepository _registrationRepository;
+
+  UpdateMedicalDetailsUsecase(
+    this._registrationRepository,
+  );
+
+  @override
+  Future<Either<BaseError, BaseInfo<T>>> execute({
+    required UpdateMedicalDetailsUsecaseParams params,
+  }) {
+    return _registrationRepository.updateMedicalDetails(
+      enquiryID: params.enquiryID,medicalDetails: params.medicalDetails
+    );
+  }
+}
+
+class UpdateMedicalDetailsUsecaseParams extends Params {
+  final String enquiryID;
+  final MedicalDetailsEntity medicalDetails;
+  UpdateMedicalDetailsUsecaseParams({
+    required this.enquiryID,
+    required this.medicalDetails
+  });
+
+  @override
+  Either<AppError, bool> verify() {
+    return Right(true);
+  }
+}
