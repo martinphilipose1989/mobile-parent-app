c22a47dfe5cbbc06a446dd0efe9e735adac5b6b4
KushDev28 <kushal.chaturvedi@neosoftmail.com>
2024-09-02 13:10:57 +0530

    PointofContact create and ui added to registration module

---
+++ infrastructure/network-retrofit/lib/src/model/response/get_registreation_info/point_of_contact_entity.dart
@@ -0,0 +1,54 @@
+
+
+import 'dart:math';
+
+import 'package:data/data.dart';
+
+import '../../../../network_retrofit.dart';
+part 'point_of_contact_entity.g.dart';
+@JsonSerializable(explicitToJson: true,createPerFieldToJson: false)
+class PointOfContactInfoEntity extends BaseLayerDataTransformer<PointOfContactInfoEntity,PointOfContactDetail>{
+  @JsonKey(name: 'contact_person')
+  String? parentType;
+  @JsonKey(name: 'mobile')
+  String? parentContactNumber;
+  @JsonKey(name: 'email')
+  String? parentEmailId;
+
+
+  PointOfContactInfoEntity({
+    this.parentType,
+    this.parentContactNumber,
+    this.parentEmailId
+  });
+
+  factory PointOfContactInfoEntity.fromJson(Map<String, dynamic> json) =>
+      _$PointOfContactInfoEntityFromJson(json);
+
+  Map<String, dynamic> toJson() => {
+    "contact_person": parentType,
+    "mobile": parentContactNumber,
+    "email": parentEmailId,
+
+  };
+
+  @override
+  PointOfContactDetail transform() {
+    PointOfContactDetail pointOfContactDetail = PointOfContactDetail();
+    pointOfContactDetail.parentType = parentType;
+    pointOfContactDetail.parentEmailId = parentEmailId;
+    pointOfContactDetail.parentContactNumber = parentContactNumber;
+
+    return pointOfContactDetail;
+  }
+
+  @override
+  PointOfContactInfoEntity restore(PointOfContactDetail data) {
+    PointOfContactInfoEntity pointOfContactInfoEntity = PointOfContactInfoEntity(
+      parentContactNumber: data.parentContactNumber,
+      parentEmailId: data.parentEmailId,
+      parentType: data.parentType
+    );
+    return pointOfContactInfoEntity;
+  }
+}
