b01f0d86a1d7ebefa46f693616d019a78de7185f
Namya <50008254@mahindra.com>
2024-11-15 12:47:22 +0530

    create intimation

--- app/lib/molecules/create_intimation/create_intimation_popup.dart
+++ app/lib/molecules/create_intimation/create_intimation_popup.dart
@@ -1,15 +1,43 @@
+import 'dart:async';
+
 import 'package:app/utils/common_widgets/common_textformfield_widget.dart';
+import 'package:app/utils/stream_builder/app_stream_builder.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class CreateIntimationPopup extends StatelessWidget {
  CreateIntimationPopup({super.key});
-  final TextEditingController _dateController = TextEditingController();
-  final ValueNotifier<String> _selectedDate = ValueNotifier<String>("");
+  final TextEditingController dateController = TextEditingController();
+  final TextEditingController noteController = TextEditingController();
+ final StreamController<String> _dateStreamController = StreamController<String>.broadcast();
+
+ Future<void> _selectDate(BuildContext context) async {
+   final DateTime? picked = await showDatePicker(
+     context: context,
+     initialDate: DateTime.now(),
+     firstDate: DateTime(2000),
+     lastDate: DateTime(2100),
+   );
+
+   if (picked != null) {
+     // Add the selected date to the stream
+     _dateStreamController.sink.add(DateFormat('dd/MM/yyyy').format(picked));
+   }
+ }
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      CommonTextFormField(showAstreik: true,hintText: 'DD/mm/yyyy',labelText: "Request Date",prefix: IconButton(onPressed: () {  }, icon: Icon(Icons.calendar_month,),),),
+   AppStreamBuilder(
+        dataBuilder: (context,date) {
+          return CommonTextFormField(controller:dateController,showAstreik: true,hintText: 'DD/MM/YYYY',labelText: "Request Date",
+            prefix: IconButton(onPressed: () { _selectDate(context); }, icon: Icon(Icons.calendar_month,),),);
+        },   stream: _dateStreamController.stream,
+     initialData: "DD/MM/YYYY",
+      ),
+
+      CommonTextFormField(showAstreik: true,controller: noteController, )
+
     ],);
   }
 }
