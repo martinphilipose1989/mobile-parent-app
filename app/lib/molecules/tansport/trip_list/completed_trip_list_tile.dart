792d5bcc10856e3ef2c2e28c2c0201f0bfd9857d
Ashraf Neo <ashraf.khan@neosoftmail.com>
2024-11-29 06:34:50 +0530

    code refactor

--- app/lib/molecules/tansport/trip_list/completed_trip_list_tile.dart
+++ app/lib/molecules/tansport/trip_list/completed_trip_list_tile.dart
@@ -1,11 +1,11 @@
+import 'package:app/molecules/tansport/trip_list/trip_tile_detail_item.dart';
 import 'package:app/themes_setup.dart';
+
 import 'package:app/utils/common_widgets/common_card_wrapper.dart';
+
 import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'trip_list_tile_header.dart';
-import 'trip_tile_detail_item.dart';
 
 class CompletedTripListTile extends StatelessWidget {
   final TripResult trip;
@@ -23,42 +23,46 @@ class CompletedTripListTile extends StatelessWidget {
           TripListTileHeader(tripStatus: "Completed", trip: trip),
           const Divider(color: AppColors.textPalerGray),
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                TripTileDetailItem(
-                    title: "Pickup point",
-                    subtitle: trip.routeStopMapping?.isEmpty ?? true
-                        ? ""
-                        : trip.routeStopMapping?.first.stop?.stopName ?? ""),
-                SizedBox(height: 24.h),
-                TripTileDetailItem(
-                    title: "Students",
-                    subtitle:
-                        "${trip.studentStopsMappings?.length ?? 0} Students"),
-              ]),
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                TripTileDetailItem(
-                    title: "Drop point",
-                    subtitle: trip.routeStopMapping?.isEmpty ?? true
-                        ? ""
-                        : trip.routeStopMapping?.last.stop?.stopName ?? ""),
-                SizedBox(height: 24.h),
-                TripTileDetailItem(
-                    title: "Action",
-                    subtitle: trip.routeType == "2"
-                        ? "Pickup"
-                        : trip.routeType == "1"
-                            ? "Drop"
-                            : ""),
-              ]),
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-                SizedBox(height: 24.h),
-                const TripTileDetailItem(title: "", subtitle: ""),
-                SizedBox(height: 24.h),
-                TripTileDetailItem(
-                    title: "Shift", subtitle: trip.shiftName ?? ''),
-              ])
+              TripTileDetailItem(
+                  title: "Pickup point",
+                  subtitle: trip.routeStopMapping?.isEmpty ?? true
+                      ? ""
+                      : trip.routeStopMapping?.first.stop?.stopName ?? ""),
+              TripTileDetailItem(
+                  title: "Drop point",
+                  subtitle: trip.routeStopMapping?.isEmpty ?? true
+                      ? ""
+                      : trip.routeStopMapping?.last.stop?.stopName ?? ""),
+              const Expanded(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(""),
+                      Text(""),
+                    ]),
+              ),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              TripTileDetailItem(
+                  title: "Students",
+                  subtitle:
+                      "${trip.studentStopsMappings?.length ?? 0} Students"),
+              TripTileDetailItem(
+                  title: "Action",
+                  subtitle: trip.routeType == '2'
+                      ? "Pickup"
+                      : trip.routeType == '1'
+                          ? "Drop"
+                          : ""),
+              TripTileDetailItem(
+                  title: "Shift", subtitle: trip.shiftName ?? ''),
             ],
           )
         ],
