60fb9041fab94f59e676c312c2ea921124450ac0
Ashraf Neo <ashraf.khan@neosoftmail.com>
2025-01-27 18:26:56 +0530

    Handled response for Unit8list when error is above 400

--- infrastructure/network-retrofit/lib/src/util/get_error.dart
+++ infrastructure/network-retrofit/lib/src/util/get_error.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:data/data.dart';
 import 'package:dio/dio.dart';
 import 'package:network_retrofit/src/model/error/error_entity.dart';
@@ -5,8 +8,16 @@ import 'package:network_retrofit/src/model/error/error_entity.dart';
 NetworkError getError({required Response apiResponse}) {
   if (apiResponse.data != null) {
     try {
+      dynamic responseData;
+      if (apiResponse.data is List<int>) {
+        responseData = apiResponse.data is List<int>
+            ? json.decode(utf8.decode(apiResponse.data)) // Decode binary data
+            : apiResponse.data;
+      }
+
+      log("responseData ===> $responseData");
       final ErrorEntity errorResponseEntity =
-          ErrorEntity.fromJson(apiResponse.data);
+          ErrorEntity.fromJson(responseData ?? apiResponse.data);
       return NetworkError(
         httpError: errorResponseEntity.errorCode ?? 1000,
         message:
